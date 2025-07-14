aae870c16b374ab404acae368df23f7ee232959d
Dakhve Kaif <kaifdaakve@gmail.com>
2025-07-15 02:17:34 +0530

    Adding code of DOM 5 challenges practice code

--- Intermediate/DomnBom.js
+++ Intermediate/DomnBom.js
@@ -17,7 +17,27 @@ document
     citiesList.firstElementChild.classList.add("highlight");
   });
 
+// challenge 3
 
-  // challenge 3
+document.getElementById("changeOrder").addEventListener("click", function () {
+  let coffeeType = document.getElementById("cofeeType");
+  coffeeType.textContent = "Espresso";
+  coffeeType.style.backgroundColor = "brown";
+  coffeeType.style.padding = "5px";
+});
+// challenge 4
 
-  document.getElementById("")
+document.getElementById("addNewItems").addEventListener("click", function () {
+  let newItem = document.createElement("li");
+  newItem.textContent = "Eggs";
+  document.getElementById("shoppingList").appendChild(newItem);
+});
+
+// challenge 5
+
+document
+  .getElementById("removeLastTask")
+  .addEventListener("click", function () {
+    let taskList = document.getElementById("taskList");
+    taskList.lastElementChild.remove();
+  });
