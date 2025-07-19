e02666edeb6e4a35d7d459aa9c1a628fac37496e
Dakhve Kaif <kaifdaakve@gmail.com>
2025-07-20 03:13:30 +0530

    Added last file for intermediate folder

---
+++ Intermediate/appM.js
@@ -0,0 +1,11 @@
+//Module import file
+
+//default import
+import multiply from "./mathsOperationsM.js";
+
+// named import
+import { add, subtract } from "./mathsOperationsM.js";
+
+console.log(multiply(10, 10));
+console.log(add(10, 10));
+console.log(subtract(100, 10));
